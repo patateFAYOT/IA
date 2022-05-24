b3d4eaa6686bafd9ff6c66dbf7f8285d2ecb0b8c
ilyes58 <55973481+ilyes58@users.noreply.github.com>
2022-05-24 16:21:56 -0400

    TP2 début crosshair

---
+++ TP2/Buckland_Chapter3-Steering Behaviors/poursuiveur.cpp
@@ -0,0 +1,9 @@
+#include "poursuiveur.h"
+
+
+
+Poursuiveur::Poursuiveur(GameWorld* world, Vector2D position, double rotation, Vector2D velocity, double mass, double max_force, double max_speed, double max_turn_rate, double scale) 
+	: Vehicle(world, position, rotation, velocity, mass, max_force, max_speed, max_turn_rate, scale)
+{
+
+}
---
+++ TP2/Buckland_Chapter3-Steering Behaviors/poursuiveur.h
@@ -0,0 +1,22 @@
+#ifndef POURSUIVEUR
+#define POURSUIVEUR
+
+#include "Vehicle.h"
+
+
+class Poursuiveur : public Vehicle {
+
+public: 
+    Poursuiveur(
+        GameWorld* world,
+        Vector2D position,
+        double    rotation,
+        Vector2D velocity,
+        double    mass,
+        double    max_force,
+        double    max_speed,
+        double    max_turn_rate,
+        double    scale);
+
+};
+#endif
